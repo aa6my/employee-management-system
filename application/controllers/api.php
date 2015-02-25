59e1286805a2f12c7c7e46ef9d6322ca22d1b8ed
metallurgical <norlihazmey89@gmail.com>
2015-02-25 10:27:49 +0800

    latest api

--- application/controllers/api.php
+++ application/controllers/api.php
@@ -1,9 +1,8 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
 require APPPATH.'/libraries/REST_Controller.php';
 
-class Api extends REST_Controller
+class Apps extends REST_Controller
 {
     public $url;
 
@@ -12,7 +11,6 @@ class Api extends REST_Controller
         parent::__construct();
         $this->load->helper('url');
         $this->url    = current_url();
-        $this->load->model('Api_model');
 
         /**
          * Set header for cross origin request(CORS)
@@ -29,28 +27,29 @@ class Api extends REST_Controller
     /************** THIS FUNCTION WILL CHANGE DEPEND ON COMPLEXITY OF CLIENT SIDE REQUESTED ****************************
      * @param   [type] [parameter]
      *          [value][table name]
-     *          eg : ems/type/employees
+     *          eg : crm/type/invoices
      *          
      * @param   [key] [parameter]
      *          [value][table primary key or unique value]
      *          if use this must be include also the val paremeter
      *          [val][parameter]
      *          [value][value for the primary key]
-     *          eg : ems/key/employee_id/val/42
+     *          eg : crm/key/invoice_id/val/42
      *
-     *          example Full url : ems/type/employees/key/employee_id/val/12
+     *          example Full url : crm/type/invoice/key/invoice_id/val/12
      *          
      * ----------------join table(optional-if dont want to use, no need to include join set paramter)---------
      * @param   [joinid] [parameter]
      *          [value][id for the table that need to join]
-     *          eg : joinid/employee_id @@ if want to add more than one join, separeted by '-' respectively
+     *          eg : joinid/invoice_id @@ if want to add more than one join, separeted by '-' respectively
      *          if use this must be include also the jointo paremeter and type parameter(mainly for first table to select)
      *          [jointo][parameter]
      *          [value][name of the table that need to join]
-     *          ex : jointo/mailbox_employees
-     *          eg : if more than one - ems/type/employees/joinid/employee_id-thread_id/jointo/employees-mailbox_employees          
+     *          ex : jointo/invoice_payments
+     *          eg : if more than one - crm/type/invoices/joinid/invoice_id-customer_id/jointo/invoice_payments-customers          
      * 
      **************************************************************************************************/
+ 
 
     function dataAll_get() 
     {
@@ -58,9 +57,9 @@ class Api extends REST_Controller
         *
         *
         */
-       
+        
 
-        if(($this->get('val') && !$this->get('key')) || ($this->get('key') && !$this->get('val')))
+        if(($this->get('val') && !$this->get('k ey')) || ($this->get('key') && !$this->get('val')))
         {
             $this->response(array('error' => 'The key parameter and value parameter must have'), 400);
         }
@@ -72,8 +71,8 @@ class Api extends REST_Controller
         }
         
         
-        $type    =  $this->get('type'); // get type of table need to fetch data eg:|employees(user/type/employees)|
-        $key     =  $this->get('key');  // UNIQUE ID in table to fetch from eg : |employees(user/type/employees/fetch/all@specified/key/employee_id)
+        $type    =  $this->get('type'); // get type of table need to fetch data eg:|customers(user/type/customers)|
+        $key     =  $this->get('key');  // UNIQUE ID in table to fetch from eg : |customers(user/type/customers/fetch/all@specified/key/customer_id)
         $table   = $type;               // asign type into table variable
         
         
@@ -90,7 +89,7 @@ class Api extends REST_Controller
             if($value!="none")  // return join table with condition applied
              {
                  $where = array($table.".".$key => $value);
-                 $data[$table] = $this->Api_model->get_data_join($table,$where, $join_to, $join_id, false, false);
+                 $data[$table] = $this->Midae_model->get_data_join($table,$where, $join_to, $join_id, false, false);
                 
              }
 
@@ -99,7 +98,7 @@ class Api extends REST_Controller
         {       
 
             /**
-             * if specified need and value for $key to fetch from eg : |employees(user/type/employees/val/2/key) == if no unique id just put none as a value
+             * if specified need and value for $key to fetch from eg : |customers(user/type/customers/val/2/key) == if no unique id just put none as a value
              * @var [type]
              */
             $value = $this->get('val');
@@ -108,14 +107,14 @@ class Api extends REST_Controller
 
              if (false !== strpos($this->url,'val')) //if have val string in url - must be include the key parameter also
              {
-
-                 $data[$table] = $this->Api_model->get_all_rows($table,$same_where, false, false, false, false);
+                 
+                 $data[$table] = $this->Midae_model->get_all_rows($table,$same_where, false, false, false, false);
 
              }
              else
              {
 
-                  $data[$table] = $this->Api_model->get_all_rows($table,false, false, false, false, false);
+                  $data[$table] = $this->Midae_model->get_all_rows($table,false, false, false, false, false);
              }
              
         
@@ -143,7 +142,7 @@ class Api extends REST_Controller
         /*========================================= END RESULT ================================================*/
     }
 
-    
+
     public function dataAll_options(){
        
        /**
@@ -154,18 +153,67 @@ class Api extends REST_Controller
         exit();
     }
 
+
+
     public function dataAll_post()
-     {
+    {
         /************** THIS FUNCTION WILL CHANGE DEPEND ON COMPLEXITY OF CLIENT SIDE REQUESTED ****************************
-         * insert into table
-         * Currently only one table only can insert at mean time
-         * Will changes time to time in order to create function for dynamic fucntion
+         * 
+         * Multiple or single table inserted
+         * Usage(client side) : 
+         * For the table : (multiple table inserted)    just seperated with '-' symbol
+         *                 (single table inserted)      just write down one name only
+         * $formData = array(
+                            array('name'=>'emi'),                   -----> data for table customers
+                            array('subject_name'=>'akhlak')         -----> data for table subjects
+                        );
+            $post = array('type'=>'customers-subjects', 'formData'=>$formData); 
+         * p/s : Data and table name must be write down in sequence order
+         * 
+         * Will changes time to time in order to create function for dynamic function
+         * PEACE
          */
-        $table     = $this->post('type'); //this is actually a table name
-        $arrayData = $this->post('formData');     
-        $insert    = $this->Api_model->insert_new_data($arrayData,$table);
         
-        $this->response(array('Respone'=> 'Success Insert into Data'), 200);
+        // get table value from client side
+        // get data value from client side
+        // set variable loop as a boolean to false for initial start
+        // set variable loop as a boolean true value if multiple table detected
+        $type      = $this->post('type');
+        $arrayData = $this->post('formData'); 
+        $loop = false;
+        // only for multiple table delete, if have this set to true        
+        if (false !== strpos($type,'-')){       // if '-' existed
+            $loop = true;                       // set the loop value to TRUE
+            $table = explode('-', $type);       // and then explode those '-' into array value
+        }
+        // if '-' symbol detected
+        // count the array size
+        // make insert function inside loop
+        // loop iteration depend on arraysize
+        if($loop == true){            
+            $bil = count($table);
+            for($i = 0; $i < $bil; $i++){
+                $doAdd = $this->Midae_model->insert_new_data($arrayData[$i],$table[$i]);                
+            }            
+                $this->response(array('Respone'=> 'Multiple table Insert into table'), 200);
+        }
+        // if no '-' detected
+        // single table inserted function will triggered
+        // use the array index 0
+        else{
+            
+            $table = $type;
+            if(!isset($arrayData[0])){
+                $data_val = $arrayData;
+            }
+            else{
+                $data_val = $arrayData[0];
+            }
+            
+            $doAdd = $this->Midae_model->insert_new_data($data_val,$table); 
+        }
+    
+        
           
     }
 
@@ -178,7 +226,7 @@ class Api extends REST_Controller
          * example : type/customer-vendors/key/customer_id-vendor_id/12-30
          * Just separate by '-' symbols to delete multiple table
          * Single table example :
-         * example : type/employees/key/employee_id/val/12
+         * example : type/customers/key/customer_id/val/12
          *
          * Multiple table delete notes :
          *     - The 'type','key' and 'val' values must be in order form respectively
@@ -213,7 +261,7 @@ class Api extends REST_Controller
 
                 $table = $type[$i];
                 $where = array($key[$i] => $val[$i]);
-                $doDelete = $this->Api_model->delete_data($table,$where);
+                $doDelete = $this->Midae_model->delete_data($table, $where);
             }
 
             $this->response(array('Multiple tables Delete Success'), 200);
@@ -222,7 +270,7 @@ class Api extends REST_Controller
         else{
                 $table = $type;
                 $where = array($key => $val);
-                $doDelete =$this->Api_model->delete_data($table, $where);
+                $doDelete = $this->Midae_model->delete_data($table, $where);
 
             $this->response(array('Single table Delete Success'), 200);
         }
@@ -240,8 +288,8 @@ class Api extends REST_Controller
         
         
         $columnToUpdate = $this->put('formData');
-        $usingCondition = array($pk => $pkVal); 
-        $kk             = $this->Api_model  ->update_data(columnToUpdate, $tableToUpdate, $usingCondition); 
+        $usingCondition = array($pk => $pkVal);        
+        $kk             = $this->Midae_model->update_data($columnToUpdate, $tableToUpdate, $usingCondition);
         $this->response(array('Requestsuccess'), 200);
     }
 
@@ -249,6 +297,74 @@ class Api extends REST_Controller
     public function send_post()
     {
         var_dump($this->request->body);
+    }
+
+
+    function dataInvoice_get() 
+    {
+        
+        
+        $data['invoices'] = $this->Midae_model->get_invoice_id();
+
+   
+
+        if($data)
+        {
+            $this->response($data, 200); // 200 being the HTTP response code
+        }
+
+        else
+        {
+            $this->response(array('error' => 'User could not be found'), 404);
+        }
+    }
+
+    public function dataChart_get(){
+        $tahun = $this->get('year');       
+        $bulan = array(1 => 'Jan',
+                       2 => 'Feb',
+                       3 => 'Mac',
+                       4 => 'Apr',
+                       5 => 'May',
+                       6 => 'June',
+                       7 => 'July',
+                       8 => 'Aug',
+                       9 => 'Sep',
+                       10 => 'Oct',
+                       11 => 'Nov',
+                       12 => 'Dec');
+
+        $data = $this->Midae_model->get_data_highchart($tahun);
+        $month = array();
+        $amount = array();
+        $amount['name'] = "Amount";
+        $textTahun = array();
+        $textTahun['tahun'] = $tahun;
+       
+
+        foreach($data as $k => $v){
+
+                if(array_key_exists($v['month'],$bulan)){
+                    
+                    $month['month'][] = $bulan[$v['month']];
+                }
+        }
+               
+                
+     
+
+        $results = array();
+        foreach($data as $key => $value){
+
+            $amount['data'][] = $value['amount'];
+        }
+
+        $results = array();       
+        array_push($results, $amount); 
+        array_push($results, $month);
+        array_push($results, $textTahun);
+       
+        $this->response($results, 200);
     }
 
 }
